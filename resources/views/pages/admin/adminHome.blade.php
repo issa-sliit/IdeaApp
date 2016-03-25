66829ffa2a1c5a164e753ff286dd44b769692e31
Asanka Jayasundara <asanka.jsndr@gmail.com>
2016-03-25 16:45:07 +0530

    init

---
+++ resources/views/pages/admin/adminHome.blade.php
@@ -0,0 +1,24 @@
+@extends('pages.adminMasterPage')
+
+@section('CSS_REF')
+    @parent
+
+@stop
+
+@section('CONTENT_HEADER')
+
+
+
+@stop
+
+@section('CONTENT')
+
+
+
+@stop
+
+
+@section('JS_REF')
+    @parent
+
+@stop
