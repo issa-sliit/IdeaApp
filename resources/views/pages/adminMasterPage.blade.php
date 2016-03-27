3e573c37aa8c5cdd28f1712e3cd9a372479fc3d9
Asanka Jayasundara <asanka.jsndr@gmail.com>
2016-03-27 16:14:15 +0530

    updated

--- resources/views/pages/adminMasterPage.blade.php
+++ resources/views/pages/adminMasterPage.blade.php
@@ -14,17 +14,21 @@
     <!-- Bootstrap 3.3.5 -->
     <link rel="stylesheet" href="{{asset('/admin/bootstrap/css/bootstrap.min.css')}}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('/admin/dist/css/font-awesome.min.css')}}">
+
+
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="{{asset('/admin/dist/css/ionicons.min.css')}}">
+
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('admin/dist/css/AdminLTE.min.css')}}">
 
     <link rel="stylesheet" href="{{asset('admin/dist/css/skins/_all-skins.min.css')}}">
     @show
 
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="/admin/dist/js/html5shiv.min.js"></script>
+
+    <script src="/admin/dist/js/respond.min.js"></script>
     <![endif]-->
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
